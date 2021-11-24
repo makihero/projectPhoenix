6818ae93eb81cf3257607fce0b96453324fa98d1
rubencallejas <rubencallejast@gmail.com>
2021-11-24 14:24:52 -0500

    segunda fase

--- app/src/main/java/com/example/phoenix/view/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/phoenix/view/ui/activities/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.phoenix.view.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.phoenix.R
+import com.example.phoenix.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        configNav()
+    }
+    fun configNav(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragContent) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bnvMenu).setupWithNavController(navController)
     }
 }
